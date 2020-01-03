84068f77178c01523f5838e2454c79f88e149da0
Davina Leong <davina.leong@outlook.com>
2020-01-03 17:58:10 +0800

    Added projects data script

---
+++ scripts/projects.js
@@ -0,0 +1,50 @@
+const project = [
+    {
+        "name": "Project Title 1",
+        "category": "Project Category",
+        "image": "placeholder-01.png",
+        "featured": true
+    },
+    {
+        "name": "Project Title 2",
+        "category": "Project Category",
+        "image": "placeholder-01.png",
+        "featured": true
+    },
+    {
+        "name": "Project Title 3",
+        "category": "Project Category",
+        "image": "placeholder-01.png",
+        "featured": true
+    },
+    {
+        "name": "Project Title 4",
+        "category": "Project Category",
+        "image": "placeholder-01.png",
+        "featured": true
+    },
+    {
+        "name": "Project Title 5",
+        "category": "Project Category",
+        "image": "placeholder-01.png",
+        "featured": true
+    },
+    {
+        "name": "Project Title 6",
+        "category": "Project Category",
+        "image": "placeholder-02.png",
+        "featured": true
+    },
+    {
+        "name": "Project Title 7",
+        "category": "Project Category",
+        "image": "placeholder-02.png",
+        "featured": true
+    },
+    {
+        "name": "Project Title 8",
+        "category": "Project Category",
+        "image": "placeholder-02.png",
+        "featured": true
+    }
+];
