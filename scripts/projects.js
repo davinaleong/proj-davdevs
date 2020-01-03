02966579f7905a4b97808158e62d6f7a679407e8
Davina Leong <davina.leong@outlook.com>
2020-01-03 18:21:39 +0800

    Populate showcase section with projects data

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,50 +1,58 @@
-const project = [
+const projects = [
     {
         "name": "Project Title 1",
         "category": "Project Category",
-        "image": "placeholder-01.png",
+        "image": "placeholder-01.jpg",
+        "link": "#",
         "featured": true
     },
     {
         "name": "Project Title 2",
         "category": "Project Category",
-        "image": "placeholder-01.png",
-        "featured": true
-    },
-    {
-        "name": "Project Title 3",
-        "category": "Project Category",
-        "image": "placeholder-01.png",
+        "image": "placeholder-01.jpg",
+        "link": "#",
         "featured": true
     },
     {
         "name": "Project Title 4",
         "category": "Project Category",
-        "image": "placeholder-01.png",
+        "image": "placeholder-01.jpg",
+        "link": "#",
+        "featured": true
+    },
+    {
+        "name": "Project Title 3",
+        "category": "Project Category",
+        "image": "placeholder-01.jpg",
+        "link": "#",
         "featured": true
     },
     {
         "name": "Project Title 5",
         "category": "Project Category",
-        "image": "placeholder-01.png",
+        "image": "placeholder-01.jpg",
+        "link": "#",
         "featured": true
     },
     {
         "name": "Project Title 6",
         "category": "Project Category",
-        "image": "placeholder-02.png",
-        "featured": true
+        "image": "placeholder-02.jpg",
+        "link": "#",
+        "featured": false
     },
     {
         "name": "Project Title 7",
         "category": "Project Category",
-        "image": "placeholder-02.png",
-        "featured": true
+        "image": "placeholder-02.jpg",
+        "link": "#",
+        "featured": false
     },
     {
         "name": "Project Title 8",
         "category": "Project Category",
-        "image": "placeholder-02.png",
-        "featured": true
+        "image": "placeholder-02.jpg",
+        "link": "#",
+        "featured": false
     }
 ];
---
+++ scripts/showcase.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    const featuredProjects = projects.filter(project => project.featured);
+    featuredProjects.sort((a, b) => {
+        const projectA = a.name.toLowerCase();
+        const projectB = b.name.toLowerCase();
+
+        let comparison = 0;
+        if (projectA > projectB) {
+            comparison = 1;
+        } else if (projectA < projectB) {
+            comparison = -1;
+        } else {
+            comparison = 0;
+        }
+
+        return comparison;
+    });
+
+    for(let i = 0; i < 4; ++i) {
+        const project = featuredProjects[i];
+        $('section.showcase div.cards').append(
+            '<div class="card">' +
+            '\t<img src="./assets/images/' + project.image + '">' +
+            '\t<div class="content">' +
+            '\t\t<h3 class="h5 mb-1"><a href="' + project.link + '">' + project.name + '</a></h3>' +
+            '\t\t<p>' + project.category + '</p>' +
+            '\t</div>' +
+            '</div>'
+        );
+    }
+    
+});
