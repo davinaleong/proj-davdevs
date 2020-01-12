f07d51255d00c09683d45b3642b42f2b881aebe3
Davina Leong <davina.leong@outlook.com>
2020-01-12 13:39:55 +0800

    Added work section

--- app/src/pages/IndexPage.js
+++ app/src/pages/IndexPage.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import LandingSection from './../sections/LandingSection';
+import WorkSection from './../sections/WorkSection';
 
 function IndexPage(props) {
     return (
         <div>
             <LandingSection
+                site={props.site}
+                renderAsset={props.renderAsset} />
+            <WorkSection
                 site={props.site}
                 renderAsset={props.renderAsset} />
         </div>
--- app/src/sections/WorkSection.js
+++ app/src/sections/WorkSection.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 function WorkSection(props) {
     return (
-        <div>Work</div>
+        <section className="work">
+            <h2 className="hidden">What Christ Jesus has equipped me with.</h2>
+            <div className="work-cell-design">
+                <h3 className="h3 text-monospace text-blue-gray mt-5 mb-3">Design</h3>
+                <div className="text-yellow mb-3"><i className="fal fa-drafting-compass fa-9x"></i></div>
+                <p className="mb-3">UX / Frontend</p>
+                <p>
+                    <button className="link link-orange" type="button">View more</button>
+                </p>
+            </div>
+            <div className="work-cell-develop">
+                <h3 className="h3 text-monospace text-blue-gray mt-5 mb-3">Develop</h3>
+                <div className="text-yellow mb-3"><i className="fal fa-laptop-code fa-9x"></i></div>
+                <p className="mb-3">PHP / JavaScript / Backend</p>
+                <p>
+                    <button className="link link-orange" type="button">View more</button>
+                </p>
+            </div>
+        </section>
     );
 }
 
