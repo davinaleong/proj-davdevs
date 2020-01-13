06ded6439fed5033bf87dfee50f5b786c8d5250b
Davina Leong <davina.leong@outlook.com>
2020-01-13 13:59:55 +0800

    Added pages with empty pages

---
+++ app/src/pages/ProjectItemPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProjectItemPage(props) {
+    return (
+        <div>
+            <h1>Project-Item Page</h1>
+        </div>
+    );
+}
+
+export default ProjectItemPage;
---
+++ app/src/pages/SkillsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SkillsPage(props) {
+    return (
+        <div>
+            <h1>Skills Page</h1>
+        </div>
+    );
+}
+
+export default SkillsPage;
