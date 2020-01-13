3d49d6860bd7f982446788114b175ab649209945
Davina Leong <davina.leong@outlook.com>
2020-01-13 23:16:45 +0800

    Minor changes to heading styles

--- app/src/sections/WorkSection.js
+++ app/src/sections/WorkSection.js
@@ -5,7 +5,7 @@ function WorkSection(props) {
         <section className="work">
             <h2 className="hidden">What Christ Jesus has equipped me with.</h2>
             <div className="work-cell-design">
-                <h3 className="h3 text-monospace text-blue-gray mt-5 mb-3">Design</h3>
+                <h3 className="h3 text-blue-gray mt-5 mb-3">Design</h3>
                 <div className="text-yellow mb-3"><i className="fal fa-drafting-compass fa-9x"></i></div>
                 <p className="mb-3">{props.renderSkills(props.site.skills.design.summarised, ' / ', '1d')}</p>
                 <p>
@@ -13,7 +13,7 @@ function WorkSection(props) {
                 </p>
             </div>
             <div className="work-cell-develop">
-                <h3 className="h3 text-monospace text-blue-gray mt-5 mb-3">Develop</h3>
+                <h3 className="h3 text-blue-gray mt-5 mb-3">Develop</h3>
                 <div className="text-yellow mb-3"><i className="fal fa-laptop-code fa-9x"></i></div>
                 <p className="mb-3">{props.renderSkills(props.site.skills.develop.summarised, ' / ', '1d')}</p>
                 <p>
