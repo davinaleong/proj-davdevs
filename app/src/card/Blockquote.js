7a636ed95eb592e6a1588006938f93c4070742d6
Davina Leong <davina.leong@outlook.com>
2020-01-15 14:35:50 +0800

    Implemented Blockquote component

---
+++ app/src/card/Blockquote.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Blockquote(props) {
+    return (
+        <blockquote>
+            <div className="text-yellow text-left">
+                <i className="fal fa-quote-left fa-2x"></i>
+            </div>
+            <div>{props.breakLines(props.text)}</div>
+            <div className="text-yellow text-right">
+                <i className="fal fa-quote-right fa-2x"></i>
+            </div>
+        </blockquote>
+    );
+}
+
+export default Blockquote;
