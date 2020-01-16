db9fb7d994956c21eb7b5729c56b7b8b073d2f7d
Davina Leong <davina.leong@outlook.com>
2020-01-16 10:23:44 +0800

    Added project details for B4 admin template

--- app/src/data/projects.js
+++ app/src/data/projects.js
@@ -37,6 +37,23 @@ const projects = [
             }
         ],
         "writeUp": "This project is built using `ReactJS`.\n\nI started by developing the template for the app first. The template is built in `HTML`, with `SASS` as its `CSS` pre-processor. Once the template is done, I then re-build the app in `ReactJS`.\n\nI use the templating phase to help me plan out the placement of elements as well as the app's styles.\n\nThis app is deployed on **Netlify**.\n\nFeel free to play around with the app! :-)\n\nYou can change the color theme, preferred translation and text size on the Settings page."
+    },
+    {
+        "uuid": uuid(),
+        "date": "2020-01-15",
+        "name": "Bootstrap 4 Admin Dashboard Template",
+        "category": "Bootstrap 4 Admin-Dashboard Template",
+        "image": "b4-admin-template.png",
+        "link": "https://davinaleong.github.io/proj-b4-admin-template/",
+        "featured": true,
+        "overview": "A simple Bootstrap 4 template ready to use out-of-the-box!",
+        "techStack": [
+            {
+                "name": "Frontend",
+                "items": ["HTML", "Bootstrap 4"]
+            }
+        ],
+        "writeUp": "This project is built using **Bootstrap 4**.\n\nI built this template with the mindset that it should be usable right out of the box, with minimal setup required. I often find myself in need of a simple, pure Bootstrap 4 template. I find many of the admin dashboard templates I've come across online overly engineered and complicated to setup and use.\n\nFeel free to use this template for your own admin-dashboard projects.\n\nThis project also comes packaged with packages I use quite often in my web-development projects.\n\nIncluded packages: `ParsleyJS` for *form validation*, `DataTables` - *sort, search and paginate table data*, `MomentJS` - *format date-times*, `Tempus Dominus` - *datetime picker*, `PrismJS` - *syntax highlighting*."
     }
 ];
 
--- app/src/loose/Card.js
+++ app/src/loose/Card.js
@@ -5,7 +5,7 @@ function Card(props) {
         <div className="card">
             <img src={props.renderAsset('image', props.project.image)} alt={props.project.name} />
             <div className="content">
-                <h3 className="h5 mb-1">
+                <h3 className="h5 text-left mb-1">
                     <button className="link link-orange" type="button" onClick={() => {props.gotoProjectItem(props.project.uuid)}}>
                         {props.project.name}
                     </button>
