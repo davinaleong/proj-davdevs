0678fb8325533a415bafb510c50347ba02ef58ba
Davina Leong <davina.leong@outlook.com>
2020-01-17 10:39:02 +0800

    Render category icon in template

--- scripts/featured.js
+++ scripts/featured.js
@@ -21,6 +21,7 @@ $(document).ready(function() {
         const project = featuredProjects[i];
         $('section.showcase div.cards').append(
             '<div class="card">' +
+            '\t<div class="icon"><i class="fab fa-html5 fa-3x" style="color: #E54C21"></i></div>' +
             '\t<img src="./assets/images/' + project.image + '">' +
             '\t<div class="content">' +
             '\t\t<h3 class="h5 mb-1"><a href="' + project.link + '">' + project.name + '</a></h3>' +
