e4c97c1eca1216b0f961774051adb780a9fcb289
Davina Leong <davina.leong@outlook.com>
2020-01-17 09:10:48 +0800

    Added a new project

--- app/src/data/projects.js
+++ app/src/data/projects.js
@@ -142,7 +142,7 @@ const projects = [
                 "items": ["CSS Grid", "Flexbox"]
             }
         ],
-        "writeUp": "..."
+        "writeUp": "This project is the first part of a 2-part project I'm in the midst of building. The second part is to build this project in `ReactJS`.\n\nI first sketched up the mockups of the app pages in [Affinity Designer](https://affinity.serif.com/en-gb/designer/).\n\nI then built the template in `HTML` and `SASS`, using the [Live Sass Compiler](https://marketplace.visualstudio.com/items?itemName=ritwickdey.live-sass) [VS Code](https://code.visualstudio.com/) extension to compile the `SASS` scripts.\n\nThis template features **CSS Grid** and **Flexbox**. **CSS Grid** is used to lay out the main components like the sidebar and main sections, and **Flexbox** is used to lay out the Team-cards.\n\n`jQuery` is used to render the Team-cards and the Team-list on the sidebar. Dummy team data is stored in a `JSON` file and is pulled in and displayed by the Team-cards."
     }
 ];
 
