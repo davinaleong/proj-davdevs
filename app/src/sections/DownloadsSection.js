7a5bc4a0925c786988fe2b27acfab3d0862c0d1c
Personal Profile <davina.leong.sy@gmail.com>
2021-02-24 12:17:58 +0800

    Updated resume urls

--- app/src/sections/DownloadsSection.js
+++ app/src/sections/DownloadsSection.js
@@ -6,11 +6,11 @@ function DownloadsSection() {
             <h3 className="text-blue-gray mb-1">Downloads</h3>
             <h5 className="text-body mb-3">Resumes</h5>
             <p>
-                <a className="text-orange button mr-3" href="https://drive.google.com/file/d/1vTfiAjJ8TBN8twhr3tfKvPm9_QxuurZs/view?usp=sharing"
+                <a className="text-orange button mr-3" href="https://drive.google.com/file/d/1Dp_q3fgfsIjjdm425OXpM51YG7NmkHU4/view?usp=sharing"
                     target="_blank" rel="noopener noreferrer">
                     <i className="fal fa-file-pdf"></i> PDF
                 </a>
-                <a className="text-orange button" href="https://drive.google.com/file/d/1Wnw6dBXV1bl1BN4zLoTosX9XDOQjVCef/view?usp=sharing"
+                <a className="text-orange button" href="https://drive.google.com/file/d/1PkYhehHOMKdxYCSwGgw9hotSmHYbzSTS/view?usp=sharing"
                     target="_blank" rel="noopener noreferrer">
                     <i className="fal fa-file-word"></i> Word
                 </a>
