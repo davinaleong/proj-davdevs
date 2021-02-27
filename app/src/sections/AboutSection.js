07b98a48c51667fdc4600c53001ab2f3d608cc61
Personal Profile <davina.leong.sy@gmail.com>
2021-02-27 12:17:12 +0800

    Updated about section

--- app/src/sections/AboutSection.js
+++ app/src/sections/AboutSection.js
@@ -7,9 +7,15 @@ function AboutSection() {
 
             <h3 className="text-blue-gray mb-1">About</h3>
 
-            <p>I am a 5-year full-stack web developer. I specialise in <code>PHP</code> and <code>Javascript</code>.</p>
-            <p>During the course of my work I've built many content-mangament dashboards in <strong>CodeIgniter</strong> and <strong>Laravel</strong>.<br/>I am the developer behind the <code>API</code> of the <strong>Klick</strong> social app (<a href="https://play.google.com/store/apps/details?id=com.klickdating.klick" target="_blank" rel="noopener noreferrer">Android</a>, <a href="https://apps.apple.com/sg/app/klick/id1438499613" target="_blank" rel="noopener noreferrer">iOS</a>).<br/>The <code>API</code> is built in <strong>NodeJS</strong> and <strong>GraphQL</strong>, using <strong>Serverless</strong> architecture.</p>
-            <p>I had to <strong>learn many technologies</strong> on my own including <strong>CodeIgniter</strong>, <strong>Ionic 2</strong>, <strong>Angular 2</strong>, <strong>Laravel</strong>, <strong>CraftCMS</strong> and<br/><strong>Docker</strong> during the course of my work at The Pixel Age and at The Shipyard Private Limited.</p>
+            <p>I have 5-6 years experience in web development. I specialise in <code>PHP</code> and <code>Javascript</code>.</p>
+
+            <p>During the course of my work I've built many APIs and content-mangament dashboards in <strong>CodeIgniter</strong> and <strong>Laravel</strong>.<br/>I am the developer behind the <code>API</code> of the <strong>Klick</strong> social app (<a href="https://play.google.com/store/apps/details?id=com.klickdating.klick" target="_blank" rel="noopener noreferrer">Android</a>, <a href="https://apps.apple.com/sg/app/klick/id1438499613" target="_blank" rel="noopener noreferrer">iOS</a>).<br/>The <code>API</code> is built in <strong>NodeJS</strong> and <strong>GraphQL</strong>, using <strong>Serverless</strong> architecture.</p>
+
+            <p>I also assist in maintaining <code>API</code> of the <strong>MOVE by <a href="https://www.liv3ly.com/" target="_blank" rel="noopener noreferrer">LIV3LY</a></strong> sports social app. (<a href="https://play.google.com/store/apps/details?id=sg.pangu.run" target="_blank" rel="noopener noreferrer">Android</a>, <a href="https://apps.apple.com/us/app/move-by-liv3ly/id1442767850" target="_blank" rel="noopener noreferrer">iOS</a>)</p>
+
+            <p>I had to <strong>learn many technologies</strong> on my own including <strong>CodeIgniter</strong>, <strong>Ionic 2</strong>, <strong>Angular 2</strong>, <strong>ReactJS</strong>, <strong>Laravel</strong>, <strong>CraftCMS</strong> and<br/><strong>Docker</strong> during the course of my work.</p>
+            
+            <p>The companies I worked at ranged from startups to agencies.</p>
         </section>
     );
 }
